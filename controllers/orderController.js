0297b324aa0a5529cde1a61be5fd8687a468c149
Maishmaina <simpledaniel.1818@gmail.com>
2021-07-07 08:20:36 +0300

    order getOrder update and delete

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -3,17 +3,36 @@ import OrderItem from "../models/orderItemModel.js";
 
 // @desc  Fetch all orders
 // @route GET /api/v1/orders
-// @access  Public
+// @access  Private
 
 const getOrders = async (req, res) => {
   try {
-    const result = await Order.find();
+    const result = await Order.find()
+      .populate("user", "name")
+      .sort({ dateOrdered: -1 });
     res.status(200).json(result);
   } catch (err) {
     res.status(500).json({ error: err, success: false });
   }
 };
 
+// @desc  Fetch order by id
+// @route GET /api/v1/orders/:id
+// @access  Private
+
+const getOrderById = async (req, res) => {
+  try {
+    const result = await Order.findById(req.params.id)
+      .populate("user", "name")
+      .populate({
+        path: "orderItems",
+        populate: { path: "product", populate: "category" },
+      });
+    res.status(200).json(result);
+  } catch (err) {
+    res.status(500).json({ error: err, success: false });
+  }
+};
 // @desc  Place an order
 // @route POST /api/v1/orders
 // @access  Public
@@ -61,5 +80,37 @@ const addOrderItems = async (req, res) => {
     res.status(201).json(createdOrder);
   }
 };
+// @desc Update an Order
+// @route PUT /api/v1/orders/:id
+// @access  Private
+const updateOrder = async (req, res) => {
+  const { status } = req.body;
 
-export { getOrders, addOrderItems };
+  const order = await Order.findById(req.params.id);
+
+  if (order) {
+    order.status = status;
+
+    const updated = await order.save();
+    res.status(201).json(updated);
+  } else {
+    res.status(404).json({ message: "Order not found" });
+  }
+};
+// @desc  DELETE order
+// @route DELETE /api/v1/orders/:id
+// @access  Private/admin
+
+const deleteOrder = async (req, res) => {
+  const deletOrder = await Order.findById(req.params.id);
+  if (deletOrder) {
+    await deletOrder.orderItems.map(async (orderItem) => {
+      await OrderItem.findByIdAndRemove(orderItem);
+    });
+    await deletOrder.remove();
+    res.status(200).json({ success: true, message: "Order Deleted" });
+  } else {
+    res.status(404).json({ success: false, message: "Order Not Found" });
+  }
+};
+export { getOrders, addOrderItems, getOrderById, updateOrder, deleteOrder };
--- routes/orderRoutes.js
+++ routes/orderRoutes.js
@@ -1,10 +1,21 @@
 import express from "express";
-import { getOrders, addOrderItems } from "../controllers/orderController.js";
-import { protect } from "../middleware/jwtSecure.js";
+import {
+  getOrders,
+  addOrderItems,
+  getOrderById,
+  updateOrder,
+  deleteOrder,
+} from "../controllers/orderController.js";
+import { admin, protect } from "../middleware/jwtSecure.js";
 
 //instantiate express
 const router = express.Router();
 
 router.route(`/`).get(protect, getOrders).post(protect, addOrderItems);
+router
+  .route("/:id")
+  .get(protect, getOrderById)
+  .put(protect, admin, updateOrder)
+  .delete(protect, admin, deleteOrder);
 
 export default router;
